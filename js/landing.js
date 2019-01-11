0ee95c2b248a16e15995791968429b61503bd614
Martavis Parker <deon7388@gmail.com>
2019-01-10 17:28:15 -0800

    Added animations per requirements

--- js/landing.js
+++ js/landing.js
@@ -1,22 +1,51 @@
 (async function() {
-    const programs = await fetchPrograms();
+    /*======================================================================*/
+    /* get data for weight loss programs in order to dynamically add markup */
+    /*======================================================================*/
+    // const programs = await fetchPrograms();
 
-    let programsMarkup = '';
-    for (let i = 0; i < programs.length; i++) {
-        const thisProgram = programs[i];
+    // let programsMarkup = '';
+    // for (let i = 0; i < programs.length; i++) {
+    //     const thisProgram = programs[i];
         
-        //using template literal but I understand it's not supported on IE 11 and below
-        programsMarkup += `
-            <div class="program-block">
-                <img alt="" src="${thisProgram.imageSrc}" />
-                <p class="program-title">${thisProgram.title}</p>
-                <p class="program-category">${thisProgram.category}</p>
-            </div>
-        `;
-    }
+    //     //using template literal but I understand it's not supported on IE 11 and below
+    //     programsMarkup += `
+    //         <div class="program-block">
+    //             <img alt="" src="${thisProgram.imageSrc}" />
+    //             <p class="program-title">${thisProgram.title}</p>
+    //             <p class="program-category">${thisProgram.category}</p>
+    //         </div>
+    //     `;
+    // }
 
     const programsGrid = document.getElementById('programs-grid');
-    programsGrid.insertAdjacentHTML('beforeend', programsMarkup);
+    // programsGrid.insertAdjacentHTML('beforeend', programsMarkup);
+
+    /*==================================================================*/
+    /*=============== set up scroll animation for programs =============*/
+    /*==================================================================*/
+    document.addEventListener('scroll', () => {
+        const scrollBuffer = 100;
+        if (window.scrollY + scrollBuffer >= programsGrid.getBoundingClientRect().top) {
+            const programsElements = document.querySelectorAll('.program-block');
+            for (let i = 0; i < programsElements.length; i++) {
+                const block = programsElements[i];
+                (function(block, i) {
+                    setTimeout(function() {
+                        block.classList.add('show');
+                    }, 300 * i);
+                })(block, i);
+            }
+        }
+    });
+
+    /*==================================================================*/
+    /*=============== set up click animation for search bar ============*/
+    /*==================================================================*/
+    const searchButton = document.querySelector('.search-button');
+    searchButton.addEventListener('click', () => {
+        searchButton.nextElementSibling.classList.add('show');
+    });
 })();
 
 function fetchPrograms() {
